d94f6883549456f2d9d5f16eaae0f7a129b7aba8
Eddy <milkeddy1@dailyview.tw>
2023-02-22 17:03:05 +0800

    feat: setupworker for msw

---
+++ src/mocks/browser.js
@@ -0,0 +1,27 @@
+import { setupWorker } from 'msw'
+import {
+  diffustionAPIs,
+  favorabilityAPIs,
+  hotkeywordAPIs,
+  interactionAPIs,
+  predictModuleAPIs,
+  reputationAPIs,
+  reputationModuleAPIs,
+  socialAPIs,
+  textlistAPIs,
+  volumeAPIs,
+} from './handlers'
+
+// eslint-disable-next-line import/prefer-default-export
+export const server = setupWorker(
+  ...diffustionAPIs,
+  ...favorabilityAPIs,
+  ...hotkeywordAPIs,
+  ...interactionAPIs,
+  ...predictModuleAPIs,
+  ...reputationAPIs,
+  ...reputationModuleAPIs,
+  ...socialAPIs,
+  ...textlistAPIs,
+  ...volumeAPIs,
+)
