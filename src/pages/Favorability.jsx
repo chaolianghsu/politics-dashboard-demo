368c82f08872c9bcd1d1da6cb4dcdcbb929e5d72
Eddy <milkeddy1@dailyview.tw>
2023-02-23 17:23:45 +0800

    feat: favorability page content

--- src/pages/Favorability.jsx
+++ src/pages/Favorability.jsx
@@ -1,6 +1,33 @@
+import { useNavigate } from 'react-router-dom'
+import { Stack } from '@mui/material'
+import EqualizerIcon from '@mui/icons-material/Equalizer'
+import ReplyIcon from '@mui/icons-material/Reply'
+
+import { HeaderBar, BlueButton } from '@/components'
+import { FavorabilityCard } from '@/containers/favorability'
+import { PostListCard } from '@/containers'
+
 function Favorability() {
+  const navigate = useNavigate()
   return (
-    <div>Favorability</div>
+    <Stack spacing={2} sx={{ paddingBottom: '5rem' }}>
+      <HeaderBar
+        text="聲譽模組 / 好感度
+        "
+        note={(
+          <BlueButton
+            onClick={() => { navigate('/reputation') }}
+          >
+            {' '}
+            <ReplyIcon />
+            返回
+          </BlueButton>
+)}
+        icon={<EqualizerIcon sx={{ fontSize: '3rem' }} />}
+      />
+      <FavorabilityCard />
+      <PostListCard />
+    </Stack>
   )
 }
 
