f6aa9f3ddd64a536e7e8d6fa1529a84e7997fbb7
Eddy <milkeddy1@dailyview.tw>
2023-02-24 17:07:43 +0800

    feat: loading component

---
+++ src/components/LoadingProgress.jsx
@@ -0,0 +1,16 @@
+import { Box, CircularProgress } from '@mui/material'
+
+function LoadingProgress() {
+  return (
+    <Box sx={{
+      zIndex: 2000, position: 'fixed', width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,.4)', top: 0, left: 0, color: '#D0D0D0',
+    }}
+    >
+      <CircularProgress color="inherit" size={100} thickness={4} />
+    </Box>
+  )
+}
+
+LoadingProgress.propTypes = {}
+
+export default LoadingProgress
