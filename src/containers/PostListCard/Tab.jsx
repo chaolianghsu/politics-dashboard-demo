a8e1f7ba317204c79f65944f8792941f35de7798
Eddy <milkeddy1@dailyview.tw>
2023-03-03 15:01:44 +0800

    feat: modify textlist data, feat tab component for textlist

---
+++ src/containers/PostListCard/Tab.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import { Tab as MuiTab, Tabs as MuiTabs, styled } from '@mui/material'
+
+const CustomTab = styled(MuiTab)({
+  minWidth: '160px',
+  color: 'rgb(132, 159, 180)',
+  '&.Mui-selected': {
+    backgroundColor: 'rgb(75, 194, 255)',
+    color: 'white',
+  },
+})
+
+const TabPropTypes = {
+  tabValue: PropTypes.number,
+  tabOnChange: PropTypes.func,
+  tabNames: PropTypes.arrayOf(PropTypes.string),
+}
+
+function Tab({ tabValue, tabOnChange, tabNames }) {
+  return (
+    <MuiTabs value={tabValue} onChange={tabOnChange}>
+      {tabNames.map((item) => <CustomTab label={item} />)}
+    </MuiTabs>
+  )
+}
+
+Tab.propTypes = TabPropTypes
+
+export default Tab
