5da12c39bc416b5c03a93fa36a35e8e8913bc0aa
Eddy <milkeddy1@dailyview.tw>
2023-03-13 11:04:14 +0800

    feat: add formal api

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 
 const testUrl = 'http://35.234.54.82'
 
+const prodUrl = 'https://api-taitung.keypo.tw'
+
 const devUrl = 'http://localhost:5173'
 
-export const baseUrl = +process.env.VITE_IS_MOCK_API ? devUrl : testUrl
+const realUrl = +process.env.VITE_IS_TEST_API ? testUrl : prodUrl
+
+export const baseUrl = +process.env.VITE_IS_MOCK_API ? devUrl : realUrl
 
 export const axiosInstance = axios.create({
   baseURL: baseUrl,
